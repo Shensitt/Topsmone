171da9124ac209c57f47e6918f7078a020c1d633
MikhailSenin <mi.senin@yandex.ru>
2023-11-03 21:44:42 +0300

    добавлены дополнительные страницы сайта

--- App/views.py
+++ App/views.py
@@ -8,3 +8,9 @@ def index_page(request):
 
 def about_page(request):
      return render(request, 'about.html')
+
+def contacts_page(request):
+     return render(request, 'contacts.html')
+
+def phones_page(request):
+     return render(request, 'phones.html')
--- Topsmone/urls.py
+++ Topsmone/urls.py
@@ -20,12 +20,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from App.views import index_page,  about_page
+from App.views import contacts_page, index_page,  about_page, phones_page
 # from App.views import index_page,navbar
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path('',index_page),
     path('admin/', admin.site.urls),
-    path('about',about_page)
+    path('about',about_page),
+    path('phones',phones_page),
+    path('contacts',contacts_page)
 ]
