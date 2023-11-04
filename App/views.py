25f3b0657bda34ae283f9661b4ffb68f057c9c5f
MikhailSenin <mi.senin@yandex.ru>
2023-11-04 11:02:19 +0300

    ссылки перемещены в файл links.html

--- App/views.py
+++ App/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def index_page(request):
-    return render(request, 'index.html')
+    return render(request, 'links.html')
 
 def about_page(request):
      return render(request, 'about.html')
